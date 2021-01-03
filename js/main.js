9180e9dd93a69b776554e4e4d251f25093bb0d8d
Johan Jorge <johanjorge0@gmail.com>
2021-01-03 10:28:42 +0100

    calculette entierement fonctionnelle au click mais pas testee

--- js/main.js
+++ js/main.js
@@ -13,104 +13,138 @@ const mathOperators = document.querySelectorAll(".operateurs");
 const equal = document.querySelector(".egal");
 const allButtons = document.getElementsByClassName("touchesCalculette");
 const resetButton = document.querySelector(".reset");
+const dot = document.querySelectorAll(".touchesBas")[1];
 
 let stringCalc = "";
 let stringArray = [];
 let total = 0;
 
-function softReset(){
+function softReset() {
     stringCalc = "";
     screenCalc.innerHTML = stringCalc;
-
 }
 
-function hardReset(){
+function hardReset() {
     softReset();
     stringArray = [];
 }
 
-function addToStringArray(){
+function addToStringArray() {
     stringArray.push(stringCalc);
     console.log(stringArray);
     softReset();
 }
+function hasNumber(s) {
+    return /\d/.test(s);
+  }
 
-mathOperators.forEach(item => {
-    item.addEventListener("click", function(e){
-        addToStringArray();
-        stringArray.push(item.innerHTML);
-        console.log(stringArray);
-})});
+dot.addEventListener("click", function(e){
+    const addDot = ".";
+    if(!stringCalc.includes(".") && hasNumber(stringCalc)){
+        stringCalc = stringCalc.concat(addDot);
+        screenCalc.innerHTML = stringCalc;
+    }
 
-one.addEventListener("click", function(e){
+});
+
+mathOperators.forEach((item) => {
+    item.addEventListener("click", function (e) {
+        if (stringCalc.length) {
+            addToStringArray();
+            if (
+                stringArray[stringArray.length - 1] !== "x" &&
+                stringArray[stringArray.length - 1] !== "/"
+            ) {
+                console.log(
+                    "this is the last character ",
+                    stringArray[stringArray.length - 1]
+                );
+                stringArray.push(item.innerHTML);
+                console.log(stringArray);
+            }
+        } else {
+            screenCalc.style.fontSize = "small";
+            screenCalc.style.color = "rgb(218, 168, 170)";
+            screenCalc.innerHTML = "use a valid input!";
+            setTimeout(function () {
+                screenCalc.style.fontSize = null;
+                screenCalc.style.color = null;
+                screenCalc.innerHTML = stringArray;
+            }, 1500);
+        }
+    });
+});
+
+one.addEventListener("click", function (e) {
     const addOne = 1;
     stringCalc = stringCalc.concat(addOne);
     screenCalc.innerHTML = stringCalc;
 });
 
-two.addEventListener("click", function(e){
+two.addEventListener("click", function (e) {
     const addTwo = 2;
     stringCalc = stringCalc.concat(addTwo);
     screenCalc.innerHTML = stringCalc;
 });
 
-three.addEventListener("click", function(e){
+three.addEventListener("click", function (e) {
     const addThree = 3;
     stringCalc = stringCalc.concat(addThree);
     screenCalc.innerHTML = stringCalc;
 });
 
-four.addEventListener("click", function(e){
+four.addEventListener("click", function (e) {
     const addFour = 4;
     stringCalc = stringCalc.concat(addFour);
     screenCalc.innerHTML = stringCalc;
 });
 
-five.addEventListener("click", function(e){
+five.addEventListener("click", function (e) {
     const addFive = 5;
     stringCalc = stringCalc.concat(addFive);
     screenCalc.innerHTML = stringCalc;
 });
 
-six.addEventListener("click", function(e){
+six.addEventListener("click", function (e) {
     const addSix = 6;
     stringCalc = stringCalc.concat(addSix);
     screenCalc.innerHTML = stringCalc;
 });
 
-seven.addEventListener("click", function(e){
+seven.addEventListener("click", function (e) {
     const addSeven = 7;
     stringCalc = stringCalc.concat(addSeven);
     screenCalc.innerHTML = stringCalc;
 });
 
-eight.addEventListener("click", function(e){
+eight.addEventListener("click", function (e) {
     const addEight = 8;
     stringCalc = stringCalc.concat(addEight);
     screenCalc.innerHTML = stringCalc;
 });
 
-nine.addEventListener("click", function(e){
+nine.addEventListener("click", function (e) {
     const addNine = 9;
     stringCalc = stringCalc.concat(addNine);
     screenCalc.innerHTML = stringCalc;
 });
 
-zero.addEventListener("click", function(e){
+zero.addEventListener("click", function (e) {
     const addZero = 0;
     stringCalc = stringCalc.concat(addZero);
     screenCalc.innerHTML = stringCalc;
 });
 
-resetButton.addEventListener("click", function(e){
+resetButton.addEventListener("click", function (e) {
     hardReset();
 });
 
-equal.addEventListener("click", function(){
+equal.addEventListener("click", function () {
     addToStringArray();
     let finalString = stringArray.join(" ");
-    total = (eval(finalString));
+    finalString = finalString.replace(/x/g, "*");
+    total = eval(finalString);
     screenCalc.innerHTML = total;
     stringArray = [];
-    stringCalc = total;
+    stringCalc = total.toString();
 });
